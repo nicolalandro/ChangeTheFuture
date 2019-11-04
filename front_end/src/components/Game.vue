365b69264560e74fa33351c561b798e5e5b4547c
Nicola Landro <nicolaxx94@live.it>
2019-11-04 20:56:51 +0100

    start creating game view

--- front_end/src/components/Game.vue
+++ front_end/src/components/Game.vue
@@ -1,8 +1,23 @@
 <template>
-    <div>
-        <p>text</p>
-        <p>{{ msg }}</p>
-    </div>
+    <section>
+        <b-steps has-navigation='true'>
+            <b-step-item label="Londra 2018" clickable="true" type="is-success">
+                <h1 class="title has-text-centered">Londra 2018</h1>
+                <b-button class="is-primary">Rivela</b-button>
+                <br>
+                <br>
+                <b-button type="is-link">Rivela Indizio</b-button>
+            </b-step-item>
+            <b-step-item label="Isole Caiman 2019" clickable="ture" type="is-dark">
+                <h1 class="title has-text-centered">Isole Caiman 2019</h1>
+                Lorem ipsum dolor sit amet.
+            </b-step-item>
+            <b-step-item label="Fiorenza 2019" clickable="ture" type="is-danger">
+                <h1 class="title has-text-centered">Isole Caiman 2019</h1>
+                Lorem ipsum dolor sit amet.
+            </b-step-item>
+        </b-steps>
+    </section>
 </template>
 
 <script>
@@ -13,4 +28,15 @@
         }
     }
 </script>
+
+<style>
+    .step-navigation {
+        position: absolute;
+        margin-left: auto;
+        margin-right: auto;
+        left: 0;
+        right: 0;
+        top: 10px;
+    }
+</style>
 
