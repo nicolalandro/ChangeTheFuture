117f36e0ec700746599f284304bcd1d3afe93eb1
Nicola Landro <nicolaxx94@live.it>
2019-11-04 20:31:16 +0100

    install bulma and test

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Game msg="test"></Game>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Vue from 'vue'
+    import Buefy from 'buefy'
+    import 'buefy/dist/buefy.css'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    Vue.use(Buefy)
+
+
+    // import HelloWorld from './components/HelloWorld.vue'
+    import Game from './components/Game.vue'
+
+    export default {
+        name: 'app',
+        components: {
+           Game
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ front_end/src/components/Game.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>text</p>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Game',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
