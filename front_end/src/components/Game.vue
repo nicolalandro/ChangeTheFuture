4f1be210e765a569773c0bd80b73745d7746483c
Nicola Landro <nicolaxx94@live.it>
2019-11-06 10:40:43 +0100

    icon

--- front_end/src/components/Game.vue
+++ front_end/src/components/Game.vue
@@ -4,7 +4,7 @@
             <b-step-item label="Londra 2018"
                          clickable="true"
                          type="is-success"
-                         icon="numeric-1">
+                         icon="ray-start-arrow">
                 <h1 class="title has-text-centered">Londra 2018</h1>
                 <b-button class="is-primary">Rivela</b-button>
                 <br>
@@ -15,7 +15,7 @@
                     label="Isole Caiman 2019"
                     clickable="ture"
                     type="is-dark"
-                    icon="numeric-2">
+                    icon="ray-vertex">
                 <h1 class="title has-text-centered">Isole Caiman 2019</h1>
                 Lorem ipsum dolor sit amet.
             </b-step-item>
@@ -23,7 +23,7 @@
                     label="Fiorenza 2019"
                     clickable="ture"
                     type="is-danger"
-                    icon="numeric-3">
+                    icon="ray-end-arrow">
                 <h1 class="title has-text-centered">Isole Caiman 2019</h1>
                 Lorem ipsum dolor sit amet.
             </b-step-item>
